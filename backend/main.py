c595d73ba37140321edf701cb68b779537d2c1b6
Jxse0 <jm.expositojavier@gmail.com>
2024-05-13 17:49:17 +0200

    init backend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from model import predict
+
 
 
 app = FastAPI()
@@ -27,3 +29,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/predict/{number}")
+def predict_number(number: float):
+    prediction = predict(number)  # Use the predict function from your model
+    return {"prediction": prediction}
