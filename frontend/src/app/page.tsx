0cd719a0bab6a0bbb2ba5f7a0ca35079f69fd044
Jxse0 <jm.expositojavier@gmail.com>
2024-05-13 16:29:34 +0200

    removed node backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,24 @@
 "use client";
 import Link from "next/link";
-import link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 async function getData() {
-  const res = await fetch(
-    "https://www.cs.toronto.edu/%7Ekriz/cifar-10-python.tar.gz"
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
+  const res = await fetch("http://localhost:3001/");
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
-  console.log("hi");
-  return res.json();
+  const data = await res.text(); // change this line
+  console.log(data);
+  return data;
 }
-export default async function Home() {
-  const data = await getData();
+
+export default function Home() {
+  const [data, setData] = useState("");
+
   useEffect(() => {
-    data;
+    getData()
+      .then((data) => setData(data))
+      .catch((err) => console.error(err));
   }, []);
 
   return <Link href="/home">Home</Link>;
