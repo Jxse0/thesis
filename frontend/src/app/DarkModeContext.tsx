0a98b963ea4d5021ee7e56a175f3153a2a47ba4e
Jxse0 <jm.expositojavier@gmail.com>
2024-06-06 11:14:41 +0200

    added darkmode

---
+++ frontend/src/app/DarkModeContext.tsx
@@ -0,0 +1,47 @@
+"use client";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+
+interface DarkModeContextProps {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const DarkModeContext = createContext<DarkModeContextProps | undefined>(
+  undefined
+);
+
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+  if (!context) {
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
+  }
+  return context;
+};
+
+export const DarkModeProvider = ({ children }: { children: ReactNode }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,21 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useDarkMode } from "./DarkModeContext"; // Adjust the import path
 
 async function getData() {
   const res = await fetch("http://127.0.0.1:8000/");
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
-  const data = await res.json(); // change this line
+  const data = await res.json();
   console.log(data[0]);
   return data;
 }
 
 export default function Home() {
   const [data, setData] = useState("");
+  const { darkMode, toggleDarkMode } = useDarkMode();
 
   useEffect(() => {
     getData()
@@ -22,12 +24,18 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <Link href="/uploadImage">Upload Image</Link>
-      <br />
-      <Link href="/record">Record</Link>
-      <br />
-      <Link href="/video">Upload Video</Link>
+    <div className="container">
+      <button className="toggle-button" onClick={toggleDarkMode}>
+        Toggle Dark Mode
+      </button>
+      <h1 className="welcome">Welcome to my Thesis Project</h1>
+      <div className="links">
+        <Link href="/uploadImage">Upload Image</Link>
+        <br />
+        <Link href="/record">Record</Link>
+        <br />
+        <Link href="/video">Upload Video</Link>
+      </div>
     </div>
   );
 }
